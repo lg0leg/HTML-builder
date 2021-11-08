ca548ded1f979ba02c80e3ae32b1c8847c70372f
lg0leg <lg.oleg1067@gmail.com>
2021-11-08 21:14:23 +0300

    feat: "copy directory" complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname,'files-copy'), (err) => {
+  if (err) copyDir();
+});
+
+
+
+copyDir();
+
+function copyDir() {
+
+  fs.readdir(path.join(__dirname,'files-copy'), (err,data) => {
+    data.forEach(file => {
+      let currentPath = path.join(__dirname, 'files-copy', path.basename(file));
+      fs.unlink(currentPath, (err) => {console.log(err);});
+    });
+  });
+
+  fs.readdir(path.join(__dirname,'files'), (err,data) => {
+    data.forEach(file => {
+      let currentPath = path.join(__dirname, 'files', path.basename(file));
+      fs.copyFile(currentPath, path.join(__dirname,'files-copy', path.basename(file)), (err) => {console.log(err);});
+    });
+  });
+
+}
 
